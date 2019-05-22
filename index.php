26d6e2ce92912d24e35dca81749d0206e017023e
Swagatika <39233686+swagatikapanda123@users.noreply.github.com>
2019-05-22 22:20:54 +0530

    committing changes

---
+++ index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Taylor Swift</title>
+	<meta charset="utf-8">
+	<meta name="viewport"  content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+ <header>
+  <div class="topnav">
+  <a href="#home">Home</a>
+  <a href="#images">Images</a>
+  <a href="#contact">Contact</a>
+  <a href="https://en.wikipedia.org/wiki/Taylor_Swift">About</a>
+</div>
+ </header>
+  
+<section>
+    
+<br><br>
+	<img src="tp.jpg" style="width: 500px; height:450px;  display: block;margin-left: auto;margin-right: auto;">
+	<h1 style="text-align: center;position: sticky;"> <b>TAYLOR ALISON SWIFT</b></h1>
+	<p style="text-align: center;">Explore all songs of Taylor..</p>
+	<p style="text-align: center;">******************************</p>
+  
+   <br>
+
+   <p>Taylor Swift's cats live a pretty lavish life. While Taylor is pretty famous, her two Scottish fold cats, Olivia Benson and Meredith Grey,are pretty famous as well.</p>
+   <p>Here is a look at Tay's cat..</p>
+   <img src="tc.webp" style="width: 200px; height:250px;"><br><br>
+   <iframe  src="https://www.youtube.com/embed/-mGRcnfjtlM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls></iframe>
+  <!-- Slideshow container -->
+  <br>
+  <h3 style="text-align: center;">GALLERY</h3>
+  <br>
+<div class="slideshow-container" id="images">
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 10</div>
+    <img src="tay.jpg" style="width:100%">
+    <div class="text">Taylor Swift</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 10</div>
+    <img src="tay2.jpg" style="width:100%">
+    <div class="text">American Singer-Songwriter</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 10</div>
+    <img src="tay3.jpg" style="width:100%">
+    <div class="text">At American Music Award</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 10</div>
+    <img src="tay4.jpg" style="width:100%">
+    <div class="text">Reputation Stadium Tour</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 10</div>
+    <img src="tay5.jpg" style="width:100%">
+    <div class="text">Bad Blood</div>
+  </div>    
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 10</div>
+    <img src="tay6.jpg" style="width:100%">
+    <div class="text">End Game</div>
+  </div>
+   <div class="mySlides fade">
+    <div class="numbertext">7 / 10</div>
+    <img src="tay7.jpg" style="width:100%">
+    <div class="text">Tour</div>
+  </div>
+   <div class="mySlides fade">
+    <div class="numbertext">8/ 10</div>
+    <img src="tay8.jpg" style="width:100%">
+    <div class="text">Rep Tour</div>
+  </div> 
+   <div class="mySlides fade">
+    <div class="numbertext">9 / 10</div>
+    <img src="tay9.jpg" style="width:100%">
+    <div class="text">Begin Again</div>
+  </div>
+   <div class="mySlides fade">
+    <div class="numbertext">10 / 10</div>
+    <img src="tay10.jpg" style="width:100%">
+    <div class="text">Happy Time</div>
+  </div>   
+<!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+  <span class="dot" onclick="currentSlide(8)"></span>
+  <span class="dot" onclick="currentSlide(9)"></span>
+  <span class="dot" onclick="currentSlide(10)"></span>              
+
+</div>
+
+  <h3 style="text-align: center;">All Albums of Taylor Swift</h3>
+  <img src="red.png" style="width: 200px; height: 200px; border: 2px solid-black;"><br>
+  <div class="dropdown">
+    <button class="dropbtn">Listen Now</button>
+    <div class="dropdown-content">
+    <a href="https://youtu.be/9x4uvxpKNmo">State of Grace</a>
+    <a href="https://youtu.be/Zlot0i3Zykw">Red</a>
+    <a href="https://youtu.be/JVunNNO4u44">All Too Well</a>
+    <a href="https://youtu.be/AgFeZr5ptV8">22</a>
+    <a href="https://youtu.be/WA4iX5D9Z64">We are never getting back together</a>
+    <a href="https://youtu.be/TsqpA5a5No8">Stay Stay Stay</a>
+    <a href="https://youtu.be/QuijXg8wm28">The Last Time</a>
+    <a href="https://youtu.be/niCVYoOguOo">Sad Beautiful Tragic</a>
+    <a href="https://youtu.be/cMPEd8m79Hw">Begin Again</a>
+
+    </div>
+  </div>
+
+    <img src="1989.jpg" style="width: 200px; height: 200px; border: 2px solid-black;"><br>
+  <div class="dropdown">
+    <button class="dropbtn">Listen Now</button>
+    <div class="dropdown-content">
+    <a href="https://youtu.be/e-ORhEE9VVg">Blank Space</a>
+    <a href="https://youtu.be/-CmadmM5cOk">Style</a>
+    <a href="https://youtu.be/nfWlot6h_JM">Shake It Off</a>
+    <a href="https://youtu.be/QcIy9NiNbmo">Bad Blood</a>
+    <a href="https://youtu.be/MWIMNAeGva4">This Love</a>
+    <a href="https://youtu.be/8_iOSKP9kUc">How You Get The Girl</a>
+
+    </div>
+  </div>
+      <img src="speaknow.png" style="width: 200px; height: 200px; border: 2px solid-black;"><br>
+  <div class="dropdown">
+    <button class="dropbtn">Listen Now</button>
+    <div class="dropdown-content">
+    <a href="https://youtu.be/XPBwXKgDTdE">Mine</a>
+    <a href="https://youtu.be/oKar-tF__ac">Sparks Fly</a>
+    <a href="https://youtu.be/QUwxKWT6m7U">Back To December</a>
+    <a href="https://youtu.be/4wUPASp2hfY">Speak Now</a>
+    <a href="https://youtu.be/JPTrHajnKBk">Dear John</a>
+    <a href="https://youtu.be/nN6VR92V70M">The Story Of Us</a>
+    <a href="https://youtu.be/IWSt64gNr30">Enchanted</a>
+    <a href="https://youtu.be/1aoS2HI5xsk">Last Kiss</a>
+    <a href="https://youtu.be/RJoA4dGbGqs">Innocent</a>
+    <a href="https://youtu.be/fq34Zy0s1mA">Never Grow Up</a>
+    <a href="https://youtu.be/2frTDb7JFBA">Long Live</a>
+  </div>
+  </div>
+        <img src="fearless.jpg"style="width:200px; height: 200px; border: 2px solid-black;"><br>
+  <div class="dropdown">
+    <button class="dropbtn">Listen Now</button>
+    <div class="dropdown-content">
+    <a href="https://youtu.be/OQpc5ZtrDw0">Jump Then Fall</a>
+    <a href="https://youtu.be/yjetFVpVYYs">Untouchable</a>
+    <a href="https://youtu.be/ptSjNWnzpjg">Fearless</a>
+    <a href="https://youtu.be/8xg3vE8Ie_E">Love Story</a>
+    <a href="https://youtu.be/D1Xr-JFLxik">White Horse</a>
+    <a href="https://youtu.be/VuNIsY6JdUw">You Belong With Me</a>
+    <a href="https://youtu.be/4nKN1kh6w00">Tell me why</a>
+    <a href="https://youtu.be/hN0a9VbxOo4">The Way I  Loved You</a>
+    <a href="https://youtu.be/l4_6eQm7RTQ">The Best Day</a>
+    <a href="https://youtu.be/2TtAd1UwfoM">Breathe</a>
+    <a href="https://youtu.be/ExdmOwsFxXw">Come In With The Rain</a>
+
+  </div>
+  </div>
+        <img src="rep.png" style="width: 200px; height: 200px; border: 2px solid-black;"><br>
+  <div class="dropdown">
+    <button class="dropbtn">Listen Now</button>
+    <div class="dropdown-content">
+    <a href="https://youtu.be/wIft-t-MQuE">Ready For It?</a>
+    <a href="https://youtu.be/dfnCAmr569k">End Game</a>
+    <a href="https://youtu.be/SCacsyVE2NE">I Did Something Bd</a>
+    <a href="https://youtu.be/lH6_NeA_mi0">Don't Blame Me</a>
+    <a href="https://youtu.be/tCXGJQYZ9JA">Delicate</a>
+    <a href="https://youtu.be/3tmd-ClpJxA">Look what you made me do</a>
+    <a href="https://youtu.be/F4ZZ-TJrWdY">Gorgeous</a>
+    <a href="https://youtu.be/A7m2a-HCVpM">King of my heart</a>
+    <a href="https://youtu.be/EJcU5zIrzM0">Gateway Car</a>
+    <a href="https://youtu.be/XUE8g_nsE9A">Dress</a>
+    <a href="https://youtu.be/bcnP5vn52Bk">Call it what you want</a>
+    <a href="https://youtu.be/r-ovztsaG-w">This is why we can't have nice things</a>
+    <a href="https://youtu.be/ZW1RtTcQC1o">so it goes..</a>
+  </div>
+  </div>  
+      
+
+
+  <!-- Contact -->
+  <div class="w3-light-grey w3-padding-large w3-padding-10 w3-margin-top" id="contact">
+    <h3 class="w3-center">Contact</h3>
+    <br>
+    <?php
+// define variables and set to empty values
+$name = $email = $message="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["Name"]);
+  $email = test_input($_POST["Email"]);
+  $message = test_input($_POST["Message"]);
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+
+
+    <form id="myform" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+      <div class="w3-section">
+        <p>Send a message here...</p>
+
+        <label>Name</label>
+        <input class="w3-input w3-border" type="text" required name="Name">
+      </div>
+      <div class="w3-section">
+        <label>Email</label>
+        <input class="w3-input w3-border" type="text" required name="Email">
+      </div>
+      <div class="w3-section">
+        <label>Message</label>
+        <input class="w3-input w3-border" required name="Message">
+      </div>
+      <button class="w3-button w3-block w3-dark-grey">Send</button>
+    </form><br>
+    <?php
+
+echo "<h2>Thank You!!</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $message;
+echo "<br>";
+?>
+
+
+  
+
+
+</section>
+    <footer>
+     <p><i class="fas fa-envelope"></i>swagatikapanda164@gmail.com</p>
+         <h3>Find me on social media</h3>
+    <a href="https://twitter.com/swagati89888210" target="_blank" class="fa fa-twitter-square"></a>
+  <a href="https://www.instagram.com/_swagatika__/" target="_blank" class="fa fa-instagram" ></a>
+    <a href="https://www.linkedin.com/in/swagatika-panda-7b0414181/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin-square"></i></a>
+    <a href="http://github.com/swagatikapanda123" target="_blank" rel="noopener noreferrer"><i class="fa fa-fw fa-github">
+    </i></a>
+    </footer>
+
+
+<script>
+  
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
